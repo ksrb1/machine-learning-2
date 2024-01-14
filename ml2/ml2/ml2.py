07fc08577fbf4779bccffb584e3da8b46fd9f442
Rein <91404701+ksrb1@users.noreply.github.com>
2024-01-14 14:25:55 +0400

    updated

--- ml2/ml2/ml2.py
+++ ml2/ml2/ml2.py
@@ -1,4 +1,7 @@
 
+##NOTE THIS COMMENTED CODE IS THE CODE THAT ALLOWED ME TO MAKE THE MACHINE LEARNING MODEL, IT WAS THEN TURNED INTO mnist_model.h5 FILE THAT THEN ALLOWED
+##THE REST OF MY CODE TO WORK WITH. JUST UNCOMMENT THE WHOLE BLOCK
+
 #from keras.datasets import mnist
 #from keras.models import Sequential
 #from keras.layers import Dense
@@ -18,15 +21,42 @@
 #y_test = np_utils.to_categorical(y_test)
 #num_classes = y_test.shape[1]
 
+
 ## 3. Build the model
 #model = Sequential()
+
+## Initialize a Sequential model in Keras. A Sequential model is a linear stack of layers.
+## Add the first Dense (fully connected) layer. This is the hidden layer.
+## 'num_pixels' is the number of neurons, equal to the number of input pixels (784 for MNIST).
+## 'input_dim=num_pixels' defines the shape of the input data (784 pixels per image).
+## 'activation='relu'' sets the ReLU (Rectified Linear Unit) activation function for this layer,
+## which helps the network learn non-linear relationships.
 #model.add(Dense(num_pixels, input_dim=num_pixels, activation='relu'))
+
+## Add the second Dense layer, which is the output layer of the network.
+## 'num_classes' is the number of output neurons, which is 10 for the MNIST dataset (digits 0-9).
+## 'activation='softmax'' sets the softmax activation function for the output layer,
+## which is used for multi-class classification. It outputs the probability distribution over the 10 classes.
 #model.add(Dense(num_classes, activation='softmax'))
 
 ## 4. Compile model
+
+## Compile the model by specifying the loss function and optimizer.
+## 'loss='categorical_crossentropy'' is the loss function commonly used for classification tasks.
+## 'optimizer='adam'' specifies the Adam optimizer, an efficient gradient descent algorithm.
+## 'metrics=['accuracy']' tells the model to report the accuracy during training and testing,
+## which is a common metric for classification tasks.
 #model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
+
 ## 5. Train the model
+## Train (fit) the model on the training data.
+## 'X_train' and 'y_train' are the input features and labels for training, respectively.
+## 'validation_data=(X_test, y_test)' uses the test set as the validation data. This helps in monitoring
+## the model's performance on unseen data during training.
+## 'epochs=10' sets the number of times the model will go through the entire training dataset.
+## 'batch_size=200' defines the number of samples per gradient update for training.
+## 'verbose=2' sets the verbosity mode - '2' will display messages one per epoch.
 #model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=200, verbose=2)
 
 ## 6. Evaluate the model
@@ -43,7 +73,7 @@ import numpy as np
 from PIL import Image, ImageDraw, ImageOps
 
 
-# Loading the trained model
+# Load your trained model
 model = load_model('mnist_model.h5')
 
 def predict_digit(canvas):
